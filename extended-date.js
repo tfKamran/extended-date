5126742b0d1d80e0f4b74ba025710043fe28091f
Kamran <team.radiant@gmail.com>
2015-08-01 20:54:41 +0530

    Reformat

--- extended-date.js
+++ extended-date.js
@@ -1,24 +1,36 @@
 Date.prototype.isToday = function() {
     var today = new Date();
 
-    return today.getYear() == this.getYear() && today.getMonth() == this.getMonth() && today.getDate() == this.getDate();
-}
+    return today.getYear() == this.getYear()
+            && today.getMonth() == this.getMonth()
+            && today.getDate() == this.getDate();
+};
 
 Date.prototype.isThisYear = function() {
-    var today = new Date();
-
-    return today.getYear() == this.getYear();
-}
+    return new Date().getYear() == this.getYear();
+};
 
 Date.prototype.getMonthInWords = function() {
-    return ["Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"][this.getMonth()];
-}
+    return ["Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec"]
+            [this.getMonth()];
+};
 
 Date.prototype.prettyPrint = function() {
     if (this.isToday())
-        return "" + this.getHours() + ":" + this.getMinutes();
+        return this.getHours() + ":" + this.getMinutes();
     else if (this.isThisYear())
-        return "" + this.getMonthInWords() + " " + this.getDate();
+        return this.getMonthInWords() + " " + this.getDate();
     else
-        return "" + this.getYear() + " " + this.getMonthInWords() + " " + this.getDate();
-}
+        return this.getMonthInWords() + " " + this.getDate() + ", " + this.getYear();
+};
